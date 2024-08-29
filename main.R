1aef3c1c66cb9bac6afabe40f524a0d65f639cae
yugahikida <HIKITARO2911@GMAIL.COM>
2024-08-29 13:31:44 +0200

    aggregate all the code in one file

--- main.R
+++ main.R
@@ -6,6 +6,8 @@ library(glmnet)
 library(kernlab)
 library(scoringRules)
 library(jtools)
+library(broom)
+library(pROC)
 set.seed(1)
 
 
@@ -16,6 +18,7 @@ dim(d)
 
 # checking for missing value
 sum(is.na(d))
+dim(d)
 # check for duplicate ID
 length(unique(d$PatientID)) == dim(d)[1]
 # delete unnecessary columns and do some processing
@@ -26,18 +29,7 @@ d <- d %>%
   rename(Caucasian = "0", African = "1", Asian = "2") %>%
   select( -c("3"))
 
-# EDA
-## histogram for selected variables 
-df_long <- d %>%
-  select() %>%
-  tidyr::gather(key = "variable", value = "value")
-
-
-ggplot(df_long, aes(x = value, fill = variable)) +
-  geom_histogram(alpha = 0.5, bins=20) +
-  facet_wrap(~variable, scale = "free") +
-  labs(x = "", y = "", title = "", subtitle = "") +
-  theme(legend.position = "none")
+dim(d)
 
 
 # split training data and test date. test data is use to evaluate and 
@@ -67,9 +59,9 @@ logi_reg <- train(Diagnosis ~., data = train,
 ## about caret::train https://topepo.github.io/caret/model-training-and-tuning.html
 
 
-# visualize significant coefficients
+## visualize significant coefficients
 final_model <- logi_reg$finalModel
-tidy_model <- tidy(final_model, conf.int = TRUE, conf.level = 0.95)
+tidy_model <- broom::tidy(final_model, conf.int = TRUE, conf.level = 0.95)
 tidy_model <- tidy_model %>% 
   filter(term %in% c("MMSE", "FunctionalAssessment", "MemoryComplaints",
                      "BehavioralProblems", "ADL"))
@@ -87,7 +79,6 @@ ggplot(tidy_model, aes(x = term, y = estimate)) +
 logi_reg_pred_prob <- logi_reg$finalModel %>%
   predict(test, type = "response")
 
-
 observed <- test$Diagnosis
 
 logi_reg_pred <- tibble(
@@ -101,6 +92,18 @@ table(logi_reg_pred$predicted, observed)
 ## accuracy
 logi_reg_pred %>%
   summarise(perc_correct = mean(observed == predicted))
+
+## ROC
+roc_obj <- roc(observed, logi_reg_pred_prob)
+roc_data <- data.frame(fpr = 1 - roc_obj$specificities, tpr = roc_obj$sensitivities)
+
+ggplot(roc_data, aes(x = fpr, y = tpr)) +
+  geom_line() +
+  geom_abline(linetype = "dashed", color = "grey") +
+  labs(title = "ROC Curve", x = "False Positive Rate (FPR)", y = "True Positive Rate (TPR)") +
+  theme_minimal()
+# Calculate AUC
+auc_value <- auc(roc_obj)
 
 # (2) logistic regression with L2 regularization
 
@@ -119,8 +122,6 @@ logi_reg_ridge_pred <- logi_reg_ridge %>%
   mutate(predicted = ifelse(True >= 0.5, "1", "0"),
          prob = True)
 
-
-
 table(logi_reg_ridge_pred$predicted, observed)
 
 logi_reg_ridge_pred %>%
@@ -129,7 +130,7 @@ logi_reg_ridge_pred %>%
 ## value of lambda
 logi_reg_ridge$bestTune
 
-# comparision with logistic regression
+## visualization of comparision with logistic regression
 coef_logistic <- as.data.frame(logi_reg$finalModel$coefficients)
 coef_ridge <- as.data.frame(as.matrix(coef(logi_reg_ridge$finalModel, s = 0.1)))
 df1 <- data.frame(Variable = rownames(coef_logistic), Coefficient = coef_logistic[,1], Model = "Logistic")
@@ -192,7 +193,8 @@ table(gp_pred$predicted, observed)
 gp_pred %>%
   summarise(perc_correct = mean(observed == predicted))
 
-# Evaluation for whole thing
+# Evaluation of all models
+## label prediction
 calculate_metrics <- function(predicted, observed) {
   predicted <- factor(predicted, levels = c(0, 1))
   observed <- factor(observed, levels = c(0, 1))
@@ -216,48 +218,50 @@ calculate_metrics <- function(predicted, observed) {
   return(metrics)
 }
 
-calculate_metrics(logi_reg_ridge_pred$predicted, observed)
-
+calculate_metrics(logi_reg_pred$predicted, observed) # logistic regression
+calculate_metrics(logi_reg_ridge_pred$predicted, observed) # Ridge logistic regression
+calculate_metrics(svm_pred$predicted, observed) # SVM
+calculate_metrics(gp_pred$predicted, observed) # GP
 
 # scoringrule
-# https://www.rdocumentation.org/packages/scoringRules/versions/1.1.1/topics/scores_binom
+## https://www.rdocumentation.org/packages/scoringRules/versions/1.1.1/topics/scores_binom
 
-# Calculate CRPS for each forecast
-crps_values <- crps(y = as.numeric(observed) - 1, family = "binom", size = 1, prob = gp_pred$prob[[2]])
-crps_values <- crps(y = as.numeric(observed) - 1, family = "binom", size = 1, prob = svm_pred$prob)
+## Calculate CRPS for each forecast
+crps_values <- crps(y = as.numeric(observed) - 1, family = "binom", size = 1, prob = logi_reg_pred$prob) # logistic regression
+crps_values <- crps(y = as.numeric(observed) - 1, family = "binom", size = 1, prob = logi_reg_ridge_pred$prob) # Ridge logistic regression
+crps_values <- crps(y = as.numeric(observed) - 1, family = "binom", size = 1, prob = svm_pred$prob) # SVM
+crps_values <- crps(y = as.numeric(observed) - 1, family = "binom", size = 1, prob = gp_pred$prob[[2]]) #GP
 
-
-# Calculate the average CRPS
+## Calculate the average CRPS
 average_crps <- mean(crps_values)
+average_crps
 
-
-# visualization of GP prior
+## visualization of GP prior
 simulate_gp_prior <- function(x, sigma_f = 1, length_scale = 1, n_samples = 5) {
-  # Define the RBF kernel function
+  ### Define the RBF kernel function
   rbf_kernel <- function(x1, x2) {
     sigma_f^2 * exp(-sum((x1 - x2)^2) / (2 * length_scale^2))
   }
   
-  # Create covariance matrix using the RBF kernel
+  ### Create covariance matrix using the RBF kernel
   cov_matrix <- outer(x, x, Vectorize(function(xi, xj) rbf_kernel(xi, xj)))
   
-  # Simulate samples from the multivariate normal distribution
+  ### Simulate samples from the multivariate normal distribution
   samples <- MASS::mvrnorm(n_samples, mu = rep(0, length(x)), Sigma = cov_matrix)
   
   return(samples)
 }
 
-# Define the input space
+## Define the input space
 x <- seq(-5, 5, length.out = 100)
 
-# Simulate GP priors with different values for the length_scale parameter
+## Simulate GP priors with different values for the length_scale parameter
 length_scales <- c(0.5, 1, 2)
 gp_samples <- lapply(length_scales, function(ls) simulate_gp_prior(x, length_scale = ls))
 
 plot_color <- rgb(64/255, 64/255, 64/255, alpha = 0.8)
 
-
-# Plot the results
+## Plot the results
 par(mfrow = c(1, length(length_scales)))
 
 for (i in 1:length(length_scales)) {
